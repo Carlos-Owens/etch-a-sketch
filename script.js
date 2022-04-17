077d066db05ca2bc82cf48cfb87d7a9e0d40f913
Carlos-Owens <clojr12@gmail.com>
2022-04-17 14:47:32 -0400

    Add grid function

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-const container = document.getElementById("#container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+const container = document.getElementById("container");
 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
+function makeRows(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  console.log(container);
 
-function makeRows(rowNum) {
-    for(let i = 0; i < rowNum; i++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    }
-}
+  for (i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement("div");
+    cell.innerText = (i + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
 
-function makeColumns(cellNum) {
-    for(let i = 0; i < rows.length; i++) {
-        for(let j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell"
-        }
-    }
-}
+makeRows(16, 16);
